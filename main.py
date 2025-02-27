ba2d21b6fbe9566754d485b7cce4797884e29271
arsalanshahs <arsalans863@deltalearns.ca>
2025-02-27 16:52:30 +0000

    Added all the pseudocodes

---
+++ main.py
@@ -0,0 +1,29 @@
+# Dinner Guest List
+
+# Define a Function to add a person to a list
+def add_jovan(dinner_list, human)
+
+
+# Define a Function to remove a person from the list
+def remove_jovan(dinner_list, human)
+
+# Define a Function to display the list
+
+
+# Create list
+
+
+# Ask the user how many guests they want to invite
+
+# Create a loop that will keep appending names to the list until the maximum number is reached (6)
+
+
+
+# Add names to the list
+
+
+
+# Print out invitations for each guest
+
+
+
