3a1b6aecddab69b5bd0b3d1afaac5d0ad18e0615
arsalanshahs <arsalans863@deltalearns.ca>
2025-03-02 00:46:01 +0000

    added the essential functions + add_guest loop
    I added the personalized and customizable functions, the invitation function, the set dinner time function, as well as making it so that there is a loop to add the max guests, it keeps appending the guests until it reaches the max amount. I used len in my code to print out the guests neatly with numbers.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ invitation = input("Enter in your invitation message (Without the Name) (Ex: You
 
 # Define a Function to add a person to a list
 def add_guest(dinner_list: list, guest: str, max_guests: int):
-    "Adds a guest to the dinner list if there is space"
+    """Adds a guest to the dinner list if there is space"""
     if len(dinner_list) < max_guests:
         dinner_list.append(guest)
         print(f"{guest} has been added to your guest list")
@@ -16,7 +16,7 @@ def add_guest(dinner_list: list, guest: str, max_guests: int):
 
 # Define a Function to remove a person from the list
 def remove_guest(dinner_list: list, guest: str):
-    "Removes a guest from the dinner list only if they exist"
+    """Removes a guest from the dinner list only if they exist"""
     if guest in dinner_list:
         dinner_list.remove(guest)
     # When the person is not in the list
@@ -28,19 +28,42 @@ def remove_guest(dinner_list: list, guest: str):
 # Define a Function to display the list
 
 def view_list(dinner_list: list):
-    "This Displays the current Dinner List"
+    """This Displays the current Dinner List"""
     if dinner_list:
         print("\n Your Dinner Guest List: ")
         for index, guest in enumerate(dinner_list, start=1):
-            print(index, guest)
+            print(f"{index}. {guest}")
 
     # When there is nobody in the list
     else:
         print("You have nobody in your list!")
 
-def print_list(dinner_list: list):
-    "This Prints out the Invitations for Each Guest"
+def print_invitations(dinner_list: list, invitation: str, dinner_time: str):
+    """This Prints out the Invitations for Each Guest"""
+    if not dinner_list:
+        print("You have no guests to invite.")
+        return
+    
+    print("\nInvitation Messages: ")
+    for guest in dinner_list:
+        print(f"Dear {guest}, {invitation} at {dinner_time}.")
 
+# The Function to set and adjust the dinner time
+
+def set_dinner_time():
+    """This Asks the user for the dinner time and also allows them to change it."""
+    time = input("Enter the dinner time (e.g., 7:00PM): ").strip()
+    while True:
+        change = input(f"The current dinner time is {time}. Do you want to adjust it? (yes/no): ")
+        if change == "yes":
+            time = input("Enter the new dinner time: ")
+        
+        elif change == "no":
+            print("Okay.")
+            break
+        else:
+            ("This is an Invalid input. Please enter a yes or no.")
+    return time
 
 
 
@@ -50,25 +73,33 @@ dinner_list = []
 while True:
     try:
         # Ask for number of guests
-        num = int(input("Enter in the amount of guests you are inviting: ").strip())
-        if num < 1:
-            print("It cannot be under 1 lol")
-        elif num > 100:
+        max_guests = int(input("Enter in the amount of guests you are inviting: ").strip())
+        if max_guests < 1:
+            print("There should be at least 1 guest...")
+        elif max_guests > 100:
             print("what the-")
             break
         else: 
             break
     except ValueError:
-        print("There's something wrong with that number..")
+        print("This is an invalid input. Please enter a number.")
+
+# Variable to set the dinner time
+dinner_time = set_dinner_time()
 
 # Ask the user how many guests they want to invite
 while True:
-    print("Options: (1) Add Guest (2) Remove Guest (3) View Guests Invited (4) Print Out Invitations (5) Exit")
+    print("Options: (1) Add Guests (2) Remove Guest (3) View Guests Invited (4) Print Out Invitations (5) Exit")
     choice = input("Enter in your choice: ")
-
+ 
+    # Add names to the list only if the max amount of guests isn't full
     if choice == "1":
-        guest = input("Enter in your Guest Name you want to Add: ")
-        dinner_list = add_guest(dinner_list, guest)
+        while len(dinner_list) < max_guests:
+            guest = input("Enter in the name of the guest to add: ").strip()
+            dinner_list = add_guest(dinner_list, guest, max_guests)
+        if len(dinner_list) == max_guests:
+            print("The guest list is full.")
+            
 
     elif choice == "2":
         guest = input("Enter in the Guest Name you want to Remove: ")
@@ -77,25 +108,22 @@ while True:
     elif choice == "3":
         view_list(dinner_list)
 
+    # Print out invitations for each guest
     elif choice == "4":
-        print("kkk")
-        break
+        print_invitations(dinner_list, invitation, dinner_time)
 
     elif choice == "5":
-        pass
+        print("Goodbye!")
+        break
 
     else:
         print("Invalid choice, please enter 1, 2, 3, 4, or 5")
 
-# Create a loop that will keep appending names to the list until the maximum number is reached (6)
 
 
 
-# Add names to the list
 
 
-
-# Print out invitations for each guest
 
 
 
